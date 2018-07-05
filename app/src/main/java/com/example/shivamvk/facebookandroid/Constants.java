e5ef332508afbcc3e50dafa1f9a2ee46d2c5b99e
shivamvk <shivam.vk529@gmail.com>
2018-07-05 14:16:02 +0530

    Changed the local server to AWS EC2

--- app/src/main/java/com/example/shivamvk/facebookandroid/Constants.java
+++ app/src/main/java/com/example/shivamvk/facebookandroid/Constants.java
@@ -2,13 +2,13 @@ package com.example.shivamvk.facebookandroid;
 
 public class Constants {
 
-    public static final String REGISTER_URL = "http://192.168.43.108:8080/FacebookAndroid/Signup.jsp";
-    public static final String LOGIN_URL = "http://192.168.43.108:8080/FacebookAndroid/login.jsp";
-    public static final String UPLOAD_IMAGE = "http://192.168.43.108:8080/FacebookAndroid/uploadImage.jsp";
-    public static final String SEARCH_QUERY = "http://192.168.43.108:8080/FacebookAndroid/searchuser.jsp";
-    public static final String SEND_FRIEND_REQUEST = "http://192.168.43.108:8080/FacebookAndroid/sendfriendrequest.jsp";
-    public static final String PENDING_FRIEND_REQUESTS = "http://192.168.43.108:8080/FacebookAndroid/pendingrequests.jsp";
-    public static final String ACCEPT_REQUEST = "http://192.168.43.108:8080/FacebookAndroid/acceptrequest.jsp";
-    public static final String UPLOAD_WALL_POST = "http://192.168.43.108:8080/FacebookAndroid/uploadwallpost.jsp";
-    public static final String GET_USER_FEED = "http://192.168.43.108:8080/FacebookAndroid/getfeed.jsp";
+    public static final String REGISTER_URL = "http://13.250.198.160:8080/FacebookAndroid/Signup.jsp";
+    public static final String LOGIN_URL = "http://13.250.198.160:8080/FacebookAndroid/login.jsp";
+    public static final String UPLOAD_IMAGE = "http://13.250.198.160:8080/FacebookAndroid/uploadImage.jsp";
+    public static final String SEARCH_QUERY = "http://13.250.198.160:8080/FacebookAndroid/searchuser.jsp";
+    public static final String SEND_FRIEND_REQUEST = "http://13.250.198.160:8080/FacebookAndroid/sendfriendrequest.jsp";
+    public static final String PENDING_FRIEND_REQUESTS = "http://13.250.198.160:8080/FacebookAndroid/pendingrequests.jsp";
+    public static final String ACCEPT_REQUEST = "http://13.250.198.160:8080/FacebookAndroid/acceptrequest.jsp";
+    public static final String UPLOAD_WALL_POST = "http://13.250.198.160:8080/FacebookAndroid/uploadwallpost.jsp";
+    public static final String GET_USER_FEED = "http://13.250.198.160:8080/FacebookAndroid/getfeed.jsp";
 }
