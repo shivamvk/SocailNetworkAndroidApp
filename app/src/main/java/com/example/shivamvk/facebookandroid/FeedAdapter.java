bd11a47d3d416d352d0783c6b427b1b9fb71ef0d
shivamvk <shivam.vk529@gmail.com>
2018-07-25 14:24:16 +0530

    added the comment layout

--- app/src/main/java/com/example/shivamvk/facebookandroid/FeedAdapter.java
+++ app/src/main/java/com/example/shivamvk/facebookandroid/FeedAdapter.java
@@ -14,6 +14,7 @@ import android.widget.ImageView;
 import android.widget.PopupMenu;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
